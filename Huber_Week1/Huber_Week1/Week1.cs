969e7b197cc93a5637a929097fbb3bd9da73f5f6
unknown <Jason@.(none)>
2012-09-04 20:09:18 -0700

    Initial add to get. First weke code is in there.

---
+++ Huber_Week1/Huber_Week1/Week1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Huber_Week1
+{
+    class Week1
+    {
+        static void Main(string[] args)
+        {
+            //I type stuff in here... typically..
+            /*
+            string myname = "Jason";
+            int i = 0 ;
+            bool istrue = false;
+
+            Console.Write(myname + i.ToString() + istrue.ToString());
+
+            Console.Read();
+        
+             */
+            //this is a single line comment....
+
+            int Age = 0;
+            Console.WriteLine("How old are you? \n");
+            Age = Convert.ToChar(Console.Read());
+            Console.WriteLine("You said you were " + Age.ToString() + " years old!");
+            Console.Read();
+        }
+    }
+}
