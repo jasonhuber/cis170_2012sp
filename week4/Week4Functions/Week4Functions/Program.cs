a263583ffb71301cf3cc1386124434f62f58a68a
Jason Huber <JasonHuber@gmail.com>
2012-09-25 20:27:59 -0700

    Added week 4 stuff.

---
+++ week4/Week4Functions/Week4Functions/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Week4Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a, b;
+            a = new int[4]{1,2,3,4};
+            b = new int[4] {3,4,5,6};
+            /*
+            Console.WriteLine("Please enter the first number to add:");
+            a = Console.ReadLine();
+            Console.WriteLine("Please enter the second number to add:");
+            b = Console.ReadLine();
+            */
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.WriteLine(Add(a[i], b[i]));
+            }
+
+            Console.Read();
+        }
+
+        private static int Add( int a,  int b)
+        {
+            //a = 22;
+            return a + b;
+        }
+      
+        private static int Add(string a, string b)
+        {
+            //a = 22;
+            int x, y;
+            if (int.TryParse(a, out x) && int.TryParse(b, out y))
+            {
+                return x + y;
+            }
+            else
+            {
+                return -99;
+            }
+            
+        }
+
+    }
+}
